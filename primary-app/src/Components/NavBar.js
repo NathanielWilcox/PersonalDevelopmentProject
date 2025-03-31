4af36d4c5e24429eeac227f9de4287e878d17ac2
Nathaniel Wilcox <wilcoxn@spu.edu>
2025-03-31 07:06:39 -0400

    Add NavBar component with links to Home, Map, and Profile pages

---
+++ primary-app/src/Components/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './NavBar.css'; // Optional: Add custom styles here
+
+const NavBar = () => {
+	return (
+		<nav className="navbar">
+			<ul>
+				<li>
+					<NavLink to="/">Home</NavLink>
+				</li>
+				<li>
+					<NavLink to="/map">Map</NavLink>
+				</li>
+				<li>
+					<NavLink to="/profile">Profile</NavLink>
+				</li>
+			</ul>
+		</nav>
+	);
+};
+
+export default NavBar;
+// This component creates a navigation bar with links to the Home, Map, and Profile pages.
